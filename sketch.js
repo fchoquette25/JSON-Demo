6443e790f17467bf55283a91e4c6ea4cd79fd8be
fchoquette25 <fchoquette25@avenues.org>
2025-01-30 20:09:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,25 +9,30 @@ function preload() {
 
 function setup() {
   createCanvas(600, 400);
-  dx = width/(weatherJSON.properties.periods.length+2)
+  background(220);
+  noLoop(); // Prevent draw from running repeatedly
   
 if (weatherJSON && weatherJSON.properties && weatherJSON.properties.periods) {
     dx = width / (weatherJSON.properties.periods.length + 2);
   
-  for (const p of weatherJSON.properties.periods) {
+   for (const p of weatherJSON.properties.periods) {
       minTemp = min(p.temperature, minTemp);
       maxTemp = max(p.temperature, maxTemp);
     }
   }
 }
 
+function draw() {
+  background(220);
+  textSize(16);
+  fill(0);
 
  if (weatherJSON && weatherJSON.properties && weatherJSON.properties.periods) {
     let forecast = weatherJSON.properties.periods;
     let px = dx;
     let py = map(forecast[0].temperature, minTemp, maxTemp, 0.8 * height, 0.2 * height);
     
-    for (let i = 1; i < forecast.length; i++) {
+     for (let i = 1; i < forecast.length; i++) {
       let cx = dx * (i + 1);
       let cy = map(forecast[i].temperature, minTemp, maxTemp, 0.8 * height, 0.2 * height);
       line(px, py, cx, cy);
@@ -38,4 +43,3 @@ if (weatherJSON && weatherJSON.properties && weatherJSON.properties.periods) {
     text("Failed to load weather data.", 20, 20);
   }
 }
-
